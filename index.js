e7d433171b8718db3547e6e15aca06a6d31819f2
isaiful508 <isaiful.islam508@gmail.com>
2024-05-05 19:42:15 +0600

    added bookings to database

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -9,9 +9,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
-
-
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.bhtyeej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
@@ -29,12 +26,56 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('CarDoctor').collection('services');
+    const bookingCollection = client.db('CarDoctor').collection('bookings');
+
+
+    //services
+
+    app.get('/services', async(req, res) =>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+    //extract services by id
+    app.get('/services/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) }
+
+
+      const options = {
+        
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, 
+          service_id: 1
+           },
+      };
+
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+
+
+    });
+
+
+    //insert bookings on database from ui 
+    app.post('/bookings', async(req, res) =>{
+      const booking = req.body;
+     const result = await bookingCollection.insertOne(booking);
+     res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
